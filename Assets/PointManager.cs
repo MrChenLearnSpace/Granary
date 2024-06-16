a471dc8e4619819b02785df62b5c8f7e1f0b1f3e
MrChenLearnSpace <emmaalen0@gmail.com>
2024-06-17 05:11:50 +0800

    1

---
+++ Assets/PointManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    public GameObject pointPrefab;
+    public List<Transform> points;
+    public List<Color> colors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < points.Count; i++) {
+           GameObject go = Instantiate(pointPrefab, points[i]);
+            go.GetComponent<Renderer>().material.SetColor("_MainColor",colors[i]);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
